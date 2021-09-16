f6541e79ac3e2d06d5829ed32cde95657c9a3205
0dnammoc <vini.e@outlook.com>
2021-09-16 14:08:04 -0300

    User Entity-EndResource

---
+++ src/main/java/com/odnammoc/webservicesex/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.odnammoc.webservicesex.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.odnammoc.webservicesex.entites.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "119273658", "123abc");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
