87aa4b1ec78e97a35d961288465af540b9fcb6e3
0dnammoc <vini.e@outlook.com>
2021-09-21 15:58:02 -0300

    OrderItem, manytomany association

--- src/main/java/com/odnammoc/webservicesex/config/TestConfig.java
+++ src/main/java/com/odnammoc/webservicesex/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.odnammoc.webservicesex.entites.Category;
 import com.odnammoc.webservicesex.entites.Order;
+import com.odnammoc.webservicesex.entites.OrderItem;
 import com.odnammoc.webservicesex.entites.Product;
 import com.odnammoc.webservicesex.entites.User;
 import com.odnammoc.webservicesex.entites.enums.OrderStatus;
 import com.odnammoc.webservicesex.repositories.CategoryRepository;
+import com.odnammoc.webservicesex.repositories.OrderItemRepository;
 import com.odnammoc.webservicesex.repositories.OrderRepository;
 import com.odnammoc.webservicesex.repositories.ProductRepository;
 import com.odnammoc.webservicesex.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -60,6 +65,22 @@ public class TestConfig implements CommandLineRunner {
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 	}
 
 }
